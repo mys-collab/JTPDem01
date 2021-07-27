cc80e75829bef440729e0a80decad66a830d91b1
= <=>
2021-07-27 17:55:12 +0800

    初始化项目

---
+++ src/main/java/com/jtp7/demo/OrderManageApplication.java
@@ -0,0 +1,13 @@
+package com.jtp7.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jtp7/demo/OrderManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtp7.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
