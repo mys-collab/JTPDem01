d06ccf453673c67470b3658d71dd448a42e3728f
mys <mys@qq.com>
2021-07-28 08:51:37 +0800

    修改了一些参数

--- src/main/java/com/jtp7/demo/OrderManageApplication.java
+++ src/main/java/com/jtp7/demo/OrderManageApplication.java
@@ -1,9 +1,13 @@
 package com.jtp7.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@MapperScan("com.jtp7.demo.mapper")
 public class OrderManageApplication {
 
     public static void main(String[] args) {
