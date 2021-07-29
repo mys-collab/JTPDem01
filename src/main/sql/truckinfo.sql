b4e461cb03d5695f7da6d967687185fa1416af19
mys <mys@qq.com>
2021-07-29 19:31:58 +0800

    修改了数据库的名字,和sql语句中的错误

--- src/main/sql/truckinfo.sql
+++ src/main/sql/truckinfo.sql
@@ -9,16 +9,16 @@ drop table if exists truckinfo;
 /*==============================================================*/
 /* Table: truckinfo                                             */
 /*==============================================================*/
-create table truckinfo
-(
-   id                   varchar(32) not null  comment '唯一id',
-   drivingCode          varchar(12) not null  comment '驾驶证号',
-   name                 varchar(12) not null comment '姓名',
-   phone                varchar(11) not null  comment '电话',
-   username             varchar(12) not null comment '用户名 默认为驾驶证号后6位',
-   password             varchar(12) default '123456' comment '密码',
-   email                varchar(16) comment '邮箱',
-   version              int default not null 0 comment '状态:0：在岗，1：不在岗，默认在岗',
-   primary key (id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `truckinfo` (
+                             `id` varchar(32) NOT NULL COMMENT '唯一id',
+                             `drivingCode` varchar(12) DEFAULT NULL COMMENT '驾驶证号',
+                             `name` varchar(12) DEFAULT NULL COMMENT '姓名',
+                             `phone` varchar(11) DEFAULT NULL COMMENT '电话',
+                             `username` varchar(12) DEFAULT NULL COMMENT '用户名 默认为驾驶证号后6位',
+                             `password` varchar(12) DEFAULT '123456' COMMENT '密码',
+                             `email` varchar(16) DEFAULT NULL COMMENT '邮箱',
+                             `version` int(11) DEFAULT '0' COMMENT '状态:0：在岗，1：不在岗，默认在岗',
+                             PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
